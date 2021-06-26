d2a70a393fd13306fc01fd4e1c1e2fec6ed24621
Daniel Arrais <danielarraiscarvalho@gmail.com>
2021-06-26 00:59:38 -0300

    Cria tela de autenticação

---
+++ src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import logoGoogleImg from '../assets/images/google-icon.svg';
+import Button from '../components/Button'
+
+import '../styles/auth.scss';
+
+export function Home() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Criei salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência ao-vivo</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <button className="create-room">
+                        <img src={logoGoogleImg} alt="Logo do google" />
+                        Crie sua sala com o google
+                    </button>
+                    <div className="separator">ou entre em uma sala</div>
+                    <form>
+                        <input type="text" placeholder="Digite o código da sala" />
+                        <Button type="submit">Entrar na sala</Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
