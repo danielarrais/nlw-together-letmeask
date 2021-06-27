a1be2e5394facf30ecca60390a8e59c736dd5ca9
Daniel Arrais <danielarraiscarvalho@gmail.com>
2021-06-26 21:30:13 -0300

    Salva questão e trata ausência de autenticação

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,23 +1,60 @@
+import { ChangeEvent } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { RoomCode } from '../components/RoomCode';
 import logoImg from '../assets/images/logo.svg';
 import Button from '../components/Button';
-import { RoomCode } from '../components/RoomCode';
+import useAuth from '../hooks/useAuth';
 
 import '../styles/room.scss'
+import { database } from '../services/firebase';
+import { FormEvent } from 'react';
 
 type RoomParamsType = {
     id: string
 }
 
 export default function Room() {
+    const { user, signInWithGoogle } = useAuth();
     const params = useParams<RoomParamsType>();
+    const [newQuestion, setNewQuestion] = useState('');
+    const roomId = params.id;
+
+    async function handleSendQuestion(event: FormEvent) {
+        event.preventDefault();
+
+        if (newQuestion.trim() === '') return;
+        if (!user) throw new Error('Você tem que está logado!');
+
+        const question = {
+            content: newQuestion.trim(),
+            author: {
+                name: user.name,
+                avatar: user.avatar
+            },
+            isHighlighted: false,
+            isAnswered: false
+        }
+
+        await database.ref(`rooms/${roomId}/questions`).push(question);
+
+        setNewQuestion('');
+    }
+
+    function handleQuestionInput(event: ChangeEvent<HTMLTextAreaElement>) {
+        setNewQuestion(event.target.value);
+    }
+
+    function handleLoginButton() {
+        signInWithGoogle();
+    }
 
     return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <RoomCode code={params.id} />
+                    <RoomCode code={roomId} />
                 </div>
             </header>
             <main>
@@ -26,11 +63,22 @@ export default function Room() {
                     <span>4 perguntas</span>
                 </div>
 
-                <form>
-                    <textarea placeholder="O que você quer perguntar?" />
+                <form onSubmit={handleSendQuestion}>
+                    <textarea
+                        value={newQuestion}
+                        onChange={handleQuestionInput}
+                        placeholder="O que você quer perguntar?"
+                    />
                     <div className="form-footer">
-                        <span>Para enviar uma pergunta <button>faça seu login</button>.</span>
-                        <Button type="submit">Enviar pergunta</Button>
+                        {!user ? (
+                            <span>Para enviar uma pergunta <button type="button" onClick={handleLoginButton}>faça seu login</button>.</span>
+                        ) : (
+                            <div className="user-info">
+                                <img src={user.avatar} alt={user.name} />
+                                <span>{user.name}</span>
+                            </div>
+                        )}
+                        <Button disabled={!user} type="submit">Enviar pergunta</Button>
                     </div>
                 </form>
             </main>
